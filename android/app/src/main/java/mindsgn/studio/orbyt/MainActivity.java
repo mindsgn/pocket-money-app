555164039529ba32fded481ceb889357679ccecf
mindsgn <48827267+mindsgn@users.noreply.github.com>
2023-05-27 16:05:27 +0200

    app rename

--- android/app/src/main/java/mindsgn/studio/orbyt/MainActivity.java
+++ android/app/src/main/java/mindsgn/studio/orbyt/MainActivity.java
@@ -1,4 +1,4 @@
-package seni.tembe.orbyt;
+package mindsgn.studio.orbyt;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
@@ -11,7 +11,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "orbyt";
+    return "Orbyt";
   }
 
   @Override
--- android/app/src/main/java/mindsgn/studio/orbyt/MainApplication.java
+++ android/app/src/main/java/mindsgn/studio/orbyt/MainApplication.java
@@ -1,4 +1,4 @@
-package seni.tembe.orbyt;
+package mindsgn.studio.orbyt;
 
 import android.app.Application;
 import android.content.Context;
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("seni.tembe.orbyt.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("mindsgn.studio.orbyt.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
